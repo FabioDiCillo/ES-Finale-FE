6509f4c04cf872004ff4b1c497056ce9fdf8c1e3
Fabio Di Cillo <fabiodicillo@gmail.com>
2024-07-23 15:07:04 +0200

    terminato

--- main.js
+++ main.js
@@ -45,12 +45,13 @@ function loadSingleBook(book) {
  
     bookElement.innerHTML = `
       
-    <div  class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <div  class="flex flex-col altezza items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="Migliori_libri_dal_2000.jpeg" alt="">
          <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${book.title}</h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${book.description}</p>
             <p class="mb-3 font-small text-gray-700 dark:text-gray-400">Anno: ${book.year}</p>
+            <p class="mb-3 font-small text-gray-700 dark:text-gray-400">Categoria: ${book.category.name} </p>
             <p class="mb-3 font-small text-gray-700 dark:text-gray-400">Autore: ${book.author.name} ${book.author.surname}</p>
         <div class="flex justify-between sm:gap-4">
             <button class="btn-cat"><a href="listacategorie.html">Categoria</a></button>
@@ -129,9 +130,11 @@ function filterBooksByTitle(books, searchValue) {
 
     return books.filter((book) => {
         let title = book.title.toLowerCase();
-        let description = book.description.toLowerCase(); 
+        let category = book.category.name.toLowerCase(); 
+        let author = book.author.name.toLowerCase(); 
+        let authorSurname = book.author.surname.toLowerCase(); 
 
-        return title.includes(searchValueLower) || description.includes(searchValueLower);
+        return title.includes(searchValueLower) || category.includes(searchValueLower)|| author.includes(searchValueLower) || authorSurname.includes(searchValueLower);
     });
 }
 
